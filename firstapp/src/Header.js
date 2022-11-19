11e8aa416097a1fb67a0373c134f80ac76cf54eb
Byamakesh <byamakesh.mohapatra@hotmail.com>
2022-11-19 18:41:45 +0530

    React - multiple component

---
+++ firstapp/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <div>
+            <center>
+                <h1>React App</h1>
+            </center>
+        </div>
+    )
+}
+
+export default Header
